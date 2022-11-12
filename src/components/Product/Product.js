a81936e3d4e3a78f8819fabf4c54298332031820
Saddam <saddam13@cse.pstu.ac.bd>
2022-11-12 19:29:28 +0600

    third commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const {name, img} = props.product;
+    return (
+        <div className='product'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4>{name}</h4>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import fakeData from '../../fakeData';
+import fakeData from '../../fakeData/products.json'
+import { useState } from 'react';
+import './Shop.css'
+import Product from './../Product/Product';
 
 const Shop = () => {
-    console.log(fakeData);
+    const first10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState(first10);
+    
+    console.log(first10)
     return (
-        <div>
-            <h1>This is Shop</h1>
+        <div className='shop-container'>
+            
+            <div className="product-container">
+                {
+                    products.map(product => <Product product={product} ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <h3>This is cart</h3>
+            </div>
         </div>
     );
 };
