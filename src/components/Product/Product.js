1e17067d6d2f83ba9a0dec69dd0586c29c984618
Saddam <saddam13@cse.pstu.ac.bd>
2022-11-13 14:52:41 +0600

    shop page coding done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -17,7 +17,8 @@ const Product = (props) => {
                 <p>${price}</p>
                 <br />
                 <p><small>Only {stock} left in stock - Order soon</small></p>
-                <button className='main-button'> <FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
+                <button className='main-button' onClick={() => props.handleAddProduct(props.product)}> 
+                    <FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
             </div>
 
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,10 +3,18 @@ import fakeData from '../../fakeData/products.json'
 import { useState } from 'react';
 import './Shop.css'
 import Product from './../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+    const handleAddProduct = (product) => {
+        console.log('product added', product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     
     console.log(first10)
     return (
@@ -14,11 +22,14 @@ const Shop = () => {
             
             <div className="product-container">
                 {
-                    products.map(product => <Product product={product} ></Product>)
+                    products.map(product => <Product 
+                        handleAddProduct = {handleAddProduct}
+                        product={product} ></Product>)
                 }
             </div>
             <div className="cart-container">
-                <h3>This is cart</h3>
+                
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
